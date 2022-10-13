812438b34a26c926e88f8722342863b3d8627f7a
Roger <sobadrush@icloud.com>
2022-10-13 09:35:44 +0800

    補全 TestRestTemplate 測試

--- src/test/java/com/rogerlo/demo/springmockmvctest/conrtoller/ProductControllerTest_by_TestRestTemplate.java
+++ src/test/java/com/rogerlo/demo/springmockmvctest/conrtoller/ProductControllerTest_by_TestRestTemplate.java
@@ -6,41 +6,53 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 /**
  * 使用 TestRestRemplate 進行測試時，務必先使用 SpringBoot Main 啟動應用程式
+ * 但若有設定 WebEnvironment.RANDOM_PORT，則可不用啟動 SpringBoot Main 啟動應用程式
+ *
+ * ref. https://howtodoinjava.com/spring-boot2/testing/testresttemplate-post-example/
  */
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductControllerTest_by_TestRestTemplate {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    private int randomServerPort;
 
     @BeforeEach
     void beforeEach(TestInfo testInfo){
         System.out.println("=========================【" + testInfo.getDisplayName() + "】===========================");
+        System.err.println("randomServerPort = " + randomServerPort);
     }
 
     @Test
     @DisplayName("測試使用 testRestTemplate 回應 String")
     void test_001() {
-        final String uri = "http://localhost:8080/ProductController/getAllProducts";
-        TestRestTemplate testRestTemplate = new TestRestTemplate();
-        ResponseEntity<String> response = testRestTemplate.getForEntity(uri, String.class);
+        final String urlTemplate = "http://localhost:{0}/ProductController/getAllProducts";
+        final String api_url = MessageFormat.format(urlTemplate, String.valueOf(randomServerPort));
+        ResponseEntity<String> response = this.testRestTemplate.getForEntity(api_url, String.class);
         System.out.println("response = " + response);
     }
 
     @Test
     @DisplayName("測試使用 ParameterizedTypeReference 達成泛型效果")
     void test_exchange() throws JsonProcessingException {
-        final String uri = "http://localhost:8080/ProductController/getAllProducts";
-        TestRestTemplate testRestTemplate = new TestRestTemplate();
-
-        List<ProductVO> response = testRestTemplate.exchange(uri,
+        final String urlTemplate = "http://localhost:%d/ProductController/getAllProducts";
+        final String api_url = String.format(urlTemplate, randomServerPort);
+        List<ProductVO> response = this.testRestTemplate.exchange(api_url,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ProductVO>>() {}).getBody();
